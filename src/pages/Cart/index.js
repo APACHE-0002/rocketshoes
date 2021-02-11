f6328bbcb09c1855a404d7cf6bef753301f763e6
apache-0002 <vinyy64@live.com>
2021-02-11 02:09:27 -0300

    secondcommit

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,9 +1,63 @@
 import React from 'react';
+import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md';
+
+import nike from '../../assets/nike.jpg'
+
+import './styles.css';
 
 export default function Cart(){
     return(
-        <>
-        <h1>Cart</h1>
-        </>
+        <div className="content">
+            <table>
+                <thead>
+                    <tr>
+                        <th/>
+                        <th>PRODUTO</th>
+                        <th>QTD</th>
+                        <th>SUBTOTAL</th>
+                        <th/>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <img src={nike} alt="tenis"/>
+                        </td>
+                        <td>
+                            <strong>Tênis muito massa</strong>
+                            <span>R$ 129,90</span>
+                        </td>
+                        <td>
+                            <div>
+                                <button type="button">
+                                    <MdRemoveCircleOutline size={20} color="#7159c1"/>
+                                </button>
+                                <input type="number" readOnly value={2} />
+                                <button type="button">
+                                    <MdAddCircleOutline size={20} color="#7159c1"/>
+                                </button>
+                            </div>
+                        </td>
+                        <td>
+                            <strong>R$ 258,80</strong>
+                        </td>
+                        <td>
+                            <button type="button">
+                                <MdDelete size={20} color="#7159c1" />
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <footer>
+                <button type="button">Finalizar pedido</button>
+
+                <total>
+                    <span>TOTAL</span>
+                    <strong>R$ 1920,28</strong>
+                </total>
+            </footer>
+        </div>
     )
 }
